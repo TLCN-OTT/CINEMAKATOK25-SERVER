8d431d1a9244ce15a0279aca9a0c5ba02f5f2e4b
abcdefghuy <nguyensanghuypy@gmail.com>
2025-09-17 14:07:51 +0700

    test cicd

--- db/seed.config.ts
+++ db/seed.config.ts
@@ -1,23 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import * as config from 'config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-// Helper function to get config value and handle function case
-const getConfigValue = (key: string): string | number => {
-  const value = config.get(key);
-  if (typeof value === 'function') {
-    return (value as () => string | number)();
-  }
-  return value as string | number;
-};
+import { getConfig } from '@app/common/utils/get-config';
 
 const dataSourceOptions: DataSourceOptions = {
-  type: getConfigValue('core.database.type') as any,
-  host: getConfigValue('core.database.host') as string,
-  port: getConfigValue('core.database.port') as number,
-  username: getConfigValue('core.database.username') as string,
-  password: getConfigValue('core.database.password') as string,
-  database: getConfigValue('core.database.dbName') as string,
+  type: getConfig('core.database.type', 'postgres') as any,
+  host: getConfig('core.database.host', 'localhost') as string,
+  port: getConfig('core.database.port', 5432) as number,
+  username: getConfig('core.database.username', 'user') as string,
+  password: getConfig('core.database.password', 'password') as string,
+  database: getConfig('core.database.dbName', 'dbname') as string,
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['db/migrations/*{.ts,.js}'],
   synchronize: false,
--- libs/core/src/axios/axios.service.ts
+++ libs/core/src/axios/axios.service.ts
@@ -1,86 +1,98 @@
-import { BadRequestException, Injectable, Logger } from "@nestjs/common";
-import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from "axios";
-import * as config from 'config'
 import * as https from 'https';
+
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import * as config from 'config';
 import * as crypto from 'crypto';
+
+import { getConfig } from '@app/common/utils/get-config';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+
 @Injectable()
 export class AxiosService {
-    private axiosInstance: AxiosInstance;
-    private readonly logger = new Logger(AxiosService.name); 
-    
-    constructor(){
-        const agent = new https.Agent({
-            rejectUnauthorized: false,
-            secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT ,
-        });
-        this.axiosInstance = axios.create({
-            timeout: config.get('core.axios.timeout'),
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            httpsAgent: agent
-        });
-    }
-    async get<T>(endpoint: string, config?: AxiosRequestConfig): Promise<T>{
-        try {
-            const response: AxiosResponse<T> = await this.axiosInstance.get(endpoint, config);
-            return response.data;
-        } catch (error) {
-            this.handleError(error);
-        }
-    }
-    async getCustom<T>(endpoint: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>{
-        try {
-            const response: AxiosResponse<T> = await this.axiosInstance.get(endpoint, config);
-            return response;
-        } catch (error) {
-            return error.response;
-        }
-    }
-    async post<T>(endpoint: string, data: any, config?: AxiosRequestConfig): Promise<T>{
-        try {
-            const response: AxiosResponse<T> = await this.axiosInstance.post(endpoint, data, config);
-            return response.data;
-        } catch (error) {
-            this.handleError(error);
-        }
-    }
-    async postCustom<T>(endpoint: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>{
-        try {
-            const response: AxiosResponse<T> = await this.axiosInstance.post(endpoint, data, config);
-            return response;
-        } catch (error) {
-            return error.response ?? error;
-        }
-    }
+  private axiosInstance: AxiosInstance;
+  private readonly logger = new Logger(AxiosService.name);
 
-    async put<T>(endpoint: string, data: any, config?: AxiosRequestConfig): Promise<T>{
-        try {
-            const response: AxiosResponse<T> = await this.axiosInstance.put(endpoint, data, config);
-            return response.data;
-        } catch (error) {
-            this.handleError(error);
-        }
+  constructor() {
+    const agent = new https.Agent({
+      rejectUnauthorized: false,
+      secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    });
+    this.axiosInstance = axios.create({
+      timeout: getConfig('core.axios.timeout', 3000),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      httpsAgent: agent,
+    });
+  }
+  async get<T>(endpoint: string, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.get(endpoint, config);
+      return response.data;
+    } catch (error) {
+      this.handleError(error);
     }
-    async putCustom<T>(endpoint: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>>{
-        try {
-            const response: AxiosResponse<T> = await this.axiosInstance.put(endpoint, data, config);
-            return response;
-        } catch (error) {
-            return error.response ;
-        }
+  }
+  async getCustom<T>(endpoint: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.get(endpoint, config);
+      return response;
+    } catch (error) {
+      return error.response;
     }
-    async delete<T>(endpoint: string, config?: AxiosRequestConfig): Promise<T>{ 
-        try {
-            const response: AxiosResponse<T> = await this.axiosInstance.delete(endpoint, config);
-            return response.data;
-        } catch (error) {
-            this.handleError(error);
-        }
+  }
+  async post<T>(endpoint: string, data: any, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.post(endpoint, data, config);
+      return response.data;
+    } catch (error) {
+      this.handleError(error);
     }
+  }
+  async postCustom<T>(
+    endpoint: string,
+    data: any,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse<T>> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.post(endpoint, data, config);
+      return response;
+    } catch (error) {
+      return error.response ?? error;
+    }
+  }
 
-    private handleError(error: any): never{
-        this.logger.error('Axios error:', error.response?.data || error.message || error);
-        throw new BadRequestException(error.response?.data || error.message || 'unknown error');
+  async put<T>(endpoint: string, data: any, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.put(endpoint, data, config);
+      return response.data;
+    } catch (error) {
+      this.handleError(error);
     }
+  }
+  async putCustom<T>(
+    endpoint: string,
+    data: any,
+    config?: AxiosRequestConfig,
+  ): Promise<AxiosResponse<T>> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.put(endpoint, data, config);
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  }
+  async delete<T>(endpoint: string, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.delete(endpoint, config);
+      return response.data;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  private handleError(error: any): never {
+    this.logger.error('Axios error:', error.response?.data || error.message || error);
+    throw new BadRequestException(error.response?.data || error.message || 'unknown error');
+  }
 }
